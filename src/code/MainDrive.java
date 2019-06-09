80a01230d5c29fd79e8f81ada7b48a7eb0738728
gomsu0 <sm0258@naver.com>
2019-06-09 14:50:32 +0900

    로또번호6개를 입력받기

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -13,5 +13,52 @@ public class MainDrive {
 		int[]userInputLottonum= new int[6];
 		
 		Scanner scan =new Scanner(System.in);
+		
+		for(int i =0; i<userInputLottonum.length;i++) {
+			while(true) {
+				
+				String message =String.format("%d번째숫자입력 :", i+1);
+				
+				System.out.print(message);
+				
+				int tempInput=scan.nextInt();
+				
+				boolean isRangeOK =false;
+				
+				  if(1<=tempInput && tempInput<=45) {
+					  
+					  isRangeOK = true;
+				  }
+				  boolean isDuplOk=true;
+				  
+				  for(int j=0;j<userInputLottonum.length;j++) {
+					  
+					  if(userInputLottonum[j]==tempInput) {
+						  
+						  isDuplOk=false;
+					  }
+				  }
+				  if(isRangeOK && isDuplOk) {
+					  
+					  userInputLottonum[i]=tempInput;
+					  
+					  break;
+				  }
+				  else if(!isRangeOK) {
+					  
+					  System.out.println("잘못된숫자");
+					  
+					  System.out.println("다시입력");
+				  }
+				  else if(!isDuplOk) {
+					  
+					  System.out.println("중복된숫자");
+					  
+					  System.out.println("다시입력");
+				  }
+			}
+		}
+		
+		
 	}
 }
