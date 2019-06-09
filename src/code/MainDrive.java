3c03db3f9ece9a2034880e5af05d3d79ab9901a1
gomsu0 <sm0258@naver.com>
2019-06-09 15:52:45 +0900

    오류났다만 일단올림
    번외도있음

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -9,8 +9,40 @@ public class MainDrive {
 		System.out.println("로또프로그램입니다.");
 		
 		int[] thisWeekLottoNum = {10,34,38,40,42,43};
-		
+		int thisWeekBonusNum=32;
 		int[]userInputLottonum= new int[6];
+		
+		int[]userAutoLottNum=new int[6];
+		
+		for(int i=0;i<userAutoLottNum.length;i++) {
+			while (true) {
+				int randomNum =(int) (Math.random()*45+1);
+				
+				boolean isDuplOK =true;
+				
+			   for (int num :userAutoLottNum) {
+				   if(num==randomNum) {
+					   isDuplOK=false;
+				   }
+			   }
+			   
+			   if(isDuplOK) {
+				   userAutoLottNum[i]=randomNum;
+				   break;
+			   }
+			   
+			}
+		}
+		
+		
+		for(int auto:userAutoLottNum) {
+			System.out.println(auto);
+		}
+		
+		
+		//Math.random의 결과:0<=1소수를 랜덤으로생성. 0보다크고45보다작은수를 랜덤으로할려면 *45
+		//ex)0.75124321564=>double
+		
 		
 		Scanner scan =new Scanner(System.in);
 		
@@ -74,8 +106,18 @@ public class MainDrive {
 			System.out.println("1등당첨");
 		}
 		else if(correntCount==5) {
-			System.out.println("3등당첨");
-		}
+			boolean bonusNumCorrect=false;
+			for(int i=0;i<userInputLottonum.length;i++) {
+				if(thisWeekBonusNum == userInputLottonum[i]){
+					bonusNumCorrect=true;
+				
+				}
+			}
+			if(correntCount==5) {
+				System.out.println("2등당첨");
+			}
+				
+	
 		else if(correntCount==4) {
 			System.out.println("4등당첨");
 		}
@@ -83,7 +125,9 @@ public class MainDrive {
 			System.out.println("5등당첨");
 		}
 		else  {
-			System.out.println("꽝");
-		}
+	 	System.out.println("꽝");
 	}
+  }
 }
+}
+
