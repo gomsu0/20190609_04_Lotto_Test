d3b155410577f2dadaaa03bf4ee84c7977aea53e
gomsu0 <sm0258@naver.com>
2019-06-09 14:18:59 +0900

    스캐너입력

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -1,9 +1,17 @@
 package code;
 
+import java.util.Scanner;
+
 public class MainDrive {
 	public static void main(String[] args) {
 
 		
 		System.out.println("로또프로그램입니다.");
+		
+		int[] thisWeekLottoNum = {10,34,38,40,42,43};
+		
+		int[]userInputLottonum= new int[6];
+		
+		Scanner scan =new Scanner(System.in);
 	}
 }
